5e0c77fa812b06edec17a79a75131ee3166a8079
Andrea Gobbi <agobbi@fbk.eu>
2023-03-28 10:00:35 +0200

    debug

--- dsipts/data_structure/data_structure.py
+++ dsipts/data_structure/data_structure.py
@@ -31,7 +31,6 @@ class MetricsCallback(Callback):
         
 
     def on_validation_end(self, trainer, pl_module):
-        import pdb;pdb.set_trace()
         for c in trainer.callback_metrics:
             self.metrics[c].append(trainer.callback_metrics[c].item())
 
@@ -408,11 +407,14 @@ class TimeSeries():
 
         self.dirpath = dirpath
         for c in trainer.callbacks:
-            import pdb
-            pdb.set_trace()
+
             if 'metrics' in dir(c):
                 self.losses = c.metrics
-                
+                if self.losses.shape[0]==0:
+                    print('recovering loss from csv logger, there is a bug when multiprocess are used')
+                    import pdb
+                    pdb.set_trace()
+                    
                 ##non so perche' le prime due le chiama prima del train
                 self.losses['val_loss'] = self.losses['val_loss'][2:]
                 self.losses = pd.DataFrame(self.losses)
