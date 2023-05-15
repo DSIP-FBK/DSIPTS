c2a17341b8635953b7d4da74074b91d6fc43c89d
Andrea Gobbi <agobbi@fbk.eu>
2023-05-15 11:00:43 +0200

    ag added classsification task & fix bn

--- dsipts/models/d3vae/model.py
+++ dsipts/models/d3vae/model.py
@@ -32,7 +32,9 @@ class diffusion_generate(nn.Module):
         )
         import pdb
         pdb.set_trace()
-        self.generative = Encoder( channel_mult,mult,prediction_length,num_preprocess_blocks,num_preprocess_cells,num_channels_enc,arch_instance,num_latent_per_group,num_channels_dec,groups_per_scale,num_postprocess_blocks,num_postprocess_cells,embedding_dimension,hidden_size,target_dim,sequence_length,num_layers,dropout_rate)
+        self.generative = Encoder( channel_mult,mult,sequence_length,
+                                  #prediction_length,
+                                  num_preprocess_blocks,num_preprocess_cells,num_channels_enc,arch_instance,num_latent_per_group,num_channels_dec,groups_per_scale,num_postprocess_blocks,num_postprocess_cells,embedding_dimension,hidden_size,target_dim,sequence_length,num_layers,dropout_rate)
         self.diffusion = GaussianDiffusion(
             self.generative,
             input_size=target_dim,
