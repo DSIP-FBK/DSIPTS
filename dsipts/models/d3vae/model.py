f3e2432c296e7f7bf1aea46f68eb61d3c50eb4d4
Andrea Gobbi <agobbi@fbk.eu>
2023-05-15 15:09:21 +0200

    ag added classsification task & fix bn

--- dsipts/models/d3vae/model.py
+++ dsipts/models/d3vae/model.py
@@ -116,7 +116,7 @@ class denoise_net(nn.Module):
         """
         # Embed the original time series.
         input = self.embedding(past_time_feat, mark)
-
+        input, _ = self.diffusion_gen.rnn(input)
         # Output the distribution of the generative results, the sampled generative results and the total correlations of the generative model.
         output, y_noisy, total_c, all_z = self.diffusion_gen(input, future_time_feat, t)
         import pdb
