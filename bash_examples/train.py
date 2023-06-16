0afd3ffd755a784d4ad411573c7d90327c8f23ef
Andrea Gobbi <agobbi@fbk.eu>
2023-06-16 09:25:17 +0200

    ag update

--- bash_examples/train.py
+++ bash_examples/train.py
@@ -161,6 +161,8 @@ def train(conf: DictConfig) -> None:
     split_params['past_steps'] = model_conf['past_steps']
     split_params['future_steps'] = model_conf['future_steps']
     ##save now so we can use it during the trainin step (or use intermediate pth files)
+    ts.dirpath = dirpath    
+    ts.losses = None
     ts.save(os.path.join(conf.train_config.dirpath,'model'))
 
     ##save the config for the comparison task before training so we can get predictions during the training procedure
