f85b7ad40589481eb606c72633d814db35d43dea
Andrea Gobbi <agobbi@fbk.eu>
2023-07-24 15:04:59 +0200

    ag update

--- bash_examples/load_data/load_data_incube.py
+++ bash_examples/load_data/load_data_incube.py
@@ -4,20 +4,28 @@ from dsipts import TimeSeries
 import numpy as np
 import pandas as pd
 
+ 
+
 def load_data(conf):
+
     data = pd.read_csv(os.path.join(conf.dataset.path,'data_consumption.csv'))
     data.Time = pd.to_datetime(data.Time)
     data.sort_values(by='Time',inplace=True)
-
     data_ex = data[data.PlantId==26913041]
     data_ex.rename(columns={'Time':'time'},inplace=True)
     data_ex.Value[data_ex.Value<0]=np.nan
-    data_ex = data_ex.drop_duplicates()
-    data_ex.Value = np.log(data_ex.Value+1)
+    # data_ex.Value = np.log(data_ex.Value+1)
     data_ex = data_ex.groupby('time').mean().reset_index()
     data_ex.index = data_ex.time
-    data_ex = data_ex.resample('1h').mean().reset_index()
+    # data_ex = data_ex.resample('1h').mean().reset_index()
+    empty = pd.DataFrame({'time':pd.date_range(data_ex.min(),data_ex.max(),freq='15minutes')})
+    data_ex = empty.merge(data_ex,how='left')
+    data_ex.Value = data_ex.Value.interpolate(limit=1)
     ts = TimeSeries(conf.ts.name)
-    ts.load_signal(data_ex,past_variables =['Value'],future_variables = [],target_variables =['Value'],enrich_cat=['dow','hour','month'])
+    ts.load_signal(data_ex,past_variables =['Value'],future_variables = [],target_variables =['Value'],enrich_cat=['dow','hour','month', 'minute'])
+
+ 
 
     return ts
+
+ 
