fbabe6091af0d9bc06c5bce559f5a8eb1fea6325
Andrea Gobbi <agobbi@fbk.eu>
2023-07-24 12:03:29 +0200

    ag update

--- bash_examples/load_data/load_data_incube.py
+++ bash_examples/load_data/load_data_incube.py
@@ -12,6 +12,7 @@ def load_data(conf):
     data_ex = data[data.PlantId==26913041]
     data_ex.rename(columns={'Time':'time'},inplace=True)
     data_ex.Value[data_ex.Value<0]=np.nan
+    data_ex = data_ex.drop_duplicates()
     data_ex.Value = np.log(data_ex.Value+1)
     data_ex = data_ex.groupby('time').mean().reset_index()
     data_ex.index = data_ex.time
