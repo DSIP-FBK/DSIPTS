d6c55a492bfd050ddd4a68942402f703102e4d58
Andrea Gobbi <agobbi@fbk.eu>
2023-08-01 15:45:13 +0200

    ag update

--- bash_examples/load_data/load_data_incube.py
+++ bash_examples/load_data/load_data_incube.py
@@ -7,25 +7,29 @@ import pandas as pd
  
 
 def load_data(conf):
-
+    id = 27350178
     data = pd.read_csv(os.path.join(conf.dataset.path,'data_consumption.csv'))
-    data.Time = pd.to_datetime(data.Time)
+    data.Time = pd.to_datetime(data.Time, utc=True)
     data.sort_values(by='Time',inplace=True)
-    data_ex = data[data.PlantId==26913041]
+    data_ex = data[data.PlantId==id]
     data_ex.rename(columns={'Time':'time'},inplace=True)
     data_ex.Value[data_ex.Value<0]=np.nan
-    # data_ex.Value = np.log(data_ex.Value+1)
-    #data_ex.Value = np.log(100*data_ex.Value+1)## reduce variability
-    data_ex = data_ex.groupby('time').mean().reset_index()
+
+    data_ex.drop_duplicates(inplace=True)
     data_ex.index = data_ex.time
+    data_ex.drop(columns='time',inplace=True)
     data_ex = data_ex.resample('1h').mean().reset_index()
-    
-    #empty = pd.DataFrame({'time':pd.date_range(data_ex.time.min(),data_ex.time.max(),freq='900s')})
-    empty = pd.DataFrame({'time':pd.date_range(data_ex.time.min(),data_ex.time.max(),freq='3600s')})
-    data_ex = empty.merge(data_ex,how='left')
-    data_ex.Value = data_ex.Value.interpolate(limit=1)
+
+    meteo = pd.read_csv(os.path.join(conf.dataset.path,'meteo.csv'))
+    meteo.Time = pd.to_datetime(meteo.Time, utc=True)
+    meteo.rename(columns={'Time':'time'},inplace=True)
+    data_ex = pd.merge(data_ex,meteo,how='left')
+    data_ex.Value = data_ex.Value.interpolate(limit=1)  
+    data_ex.rain = data_ex.rain.interpolate(limit=1)   
+    data_ex.temp = data_ex.temp.interpolate(limit=1)
+
     ts = TimeSeries(conf.ts.name)
-    ts.load_signal(data_ex,past_variables =['Value'],future_variables = [],target_variables =['Value'],enrich_cat= conf.ts.enrich)
+    ts.load_signal(data_ex,past_variables =['Value','rain','temp'],future_variables = ['rain','temp'],target_variables =['Value'],enrich_cat= conf.ts.enrich)
 
  
 
