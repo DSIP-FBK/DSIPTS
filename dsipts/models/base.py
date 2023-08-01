b356773affbdc598db06ae969f2aa5639fe3e1f3
Andrea Gobbi <agobbi@fbk.eu>
2023-08-01 11:34:29 +0200

    ag update

--- dsipts/models/base.py
+++ dsipts/models/base.py
@@ -151,7 +151,7 @@ class Base(pl.LightningModule):
             loss =  torch.mean(torch.abs(y_hat[:,:,:,idx]- batch['y'])*weights)
         
         elif self.loss_type=='sinkhorn_heavy':
-            sinkhorn = SinkhornDistance(eps=0.1, max_iter=500, reduction='mean')
+            sinkhorn = SinkhornDistance(eps=0.1, max_iter=100, reduction='mean')
             if self.use_quantiles==False:
                 x = y_hat[:,:,:,0]
             else:
@@ -160,13 +160,29 @@ class Base(pl.LightningModule):
             
         elif self.loss_type=='sinkhorn':
 
-            sinkhorn = SinkhornDistance(eps=0.1, max_iter=500, reduction='mean')
+            sinkhorn = SinkhornDistance(eps=0.1, max_iter=100, reduction='mean')
+            if self.use_quantiles==False:
+                x = y_hat[:,:,:,0]
+            else:
+                x = y_hat[:,:,:,1]
+            loss = sinkhorn.compute(x,batch['y'])
+            
+            
+        elif self.loss_type=='triplet':
+
+            triplet = torch.nn.TripletMarginLoss(margin=1.0, p=2)
             if self.use_quantiles==False:
                 x = y_hat[:,:,:,0]
             else:
                 x = y_hat[:,:,:,1]
             loss = sinkhorn.compute(x,batch['y'])
 
+            anchor = x
+            positive =  batch['y']
+            negative = y_persistence
+            loss = triplet(anchor, positive, negative)
+
+    
     
         else:
 
