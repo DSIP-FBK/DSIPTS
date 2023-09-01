67f727387adcde962c3fd3d0991de79579099396
Andrea Gobbi <agobbi@fbk.eu>
2023-09-01 11:42:03 +0200

    ag added stack generalization model

--- bash_examples/train_stack.py
+++ bash_examples/train_stack.py
@@ -71,14 +71,7 @@ def train_stack(conf: DictConfig) -> None:
         ff = os.path.join(conf.stack.models,'config_used')
         files = [os.path.join(ff,f) for f in os.listdir(ff)]
     else:
-        ## if we are here probably is becasue it falis to load the models from a list. There is a bug because hyrdra parse the string in an obscure way so we need to pass a string and parse it
-        import pdb
-        pdb.set_trace()
-        try:
-            
-            files = conf.stack.models.replace('["','').replace(']','').split(',')
-        except:
-            logging.info('FAILED TO LOAD MODELS')
+        logging.info('FAILED TO LOAD MODELS')
             
     predictions = None
     N_models = 0
