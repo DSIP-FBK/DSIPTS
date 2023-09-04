ac085d7001d5b2b0fab68f2124094eaef28bed14
Andrea Gobbi <agobbi@fbk.eu>
2023-09-04 14:28:16 +0200

    ag fixing stacked models

--- bash_examples/inference.py
+++ bash_examples/inference.py
@@ -86,7 +86,8 @@ def inference_stacked(conf:DictConfig,ts:TimeSeries)->List[pd.DataFrame]:
             prediction = prediction[['time','lag']+list(mapping.values())]
             predictions = pd.merge(predictions, prediction)
             
-
+    import pdb
+    pdb.set_trace()
     freq = prediction[prediction.lag==1].sort_values(by='time').time.diff()[1:].min()
 
     predictions['prediction_time'] = predictions.apply(lambda x: x.time-timedelta(seconds= x.lag*freq.seconds), axis=1)
