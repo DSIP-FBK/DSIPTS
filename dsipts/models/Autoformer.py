2680c3f2ca0c351b9de6b0ea1eed6b1e527e63d0
Andrea Gobbi <agobbi@fbk.eu>
2023-09-12 10:42:30 +0200

    ag added pathctst and autoformer

---
+++ dsipts/models/Autoformer.py
@@ -0,0 +1,215 @@
+
+from torch import  nn
+import torch
+from .base import Base
+from typing import List,Union
+from ..data_structure.utils import beauty_string
+from .utils import  get_activation
+from .autoformer.layers import AutoCorrelation, AutoCorrelationLayer, Encoder, Decoder, EncoderLayer, DecoderLayer, my_Layernorm, series_decomp,PositionalEmbedding
+
+
+  
+class Autoformer(Base):
+
+    
+    def __init__(self, 
+                 past_steps:int,
+                 future_steps:int,
+                 label_len: int,
+                 past_channels:int,
+                 future_channels:int,
+                 out_channels:int,
+                 d_model:int,
+                 embs:List[int],
+                 kernel_size:int,
+                 activation:str='torch.nn.ReLU',
+                 factor: int=5,
+                 n_head:int=1,
+                 n_layer_encoder:int=2,
+                 n_layer_decoder:int=2,
+                 hidden_size:int=1048,
+                 persistence_weight:float=0.0,
+                 loss_type: str='l1',
+                 quantiles:List[int]=[],
+                 dropout_rate:float=0.1,
+                 optim:Union[str,None]=None,
+                 optim_config:dict=None,
+                 scheduler_config:dict=None)->None:
+        """
+
+        Args:
+            past_steps (int): number of past datapoints used , not used here
+            future_steps (int): number of future lag to predict
+            label_len (int): overlap len
+            past_channels (int): number of numeric past variables, must be >0
+            future_channels (int): number of future numeric variables 
+            out_channels (int):  number of output channels
+            d_model (int):  dimension of the attention model
+            embs (List): list of the initial dimension of the categorical variables
+            embed_type (int): type of embedding
+            kernel_size (int): kernel_size
+            activation (str, optional): activation fuction function pytorch. Default torch.nn.ReLU
+            n_head (int, optional): number of heads
+            n_layer_encoder (int, optional): number of encoding layers
+            n_layer_decoder (int, optional): number of decoding layers
+            factor (int): num of routers in Cross-Dimension Stage of TSA (c) see the paper            
+            out_channels (int):  number of output channels
+            persistence_weight (float):  weight controlling the divergence from persistence model. Default 0
+            loss_type (str, optional): this model uses custom losses or l1 or mse. Custom losses can be linear_penalization or exponential_penalization. Default l1,
+            quantiles (List[int], optional): NOT USED YET
+            dropout_rate (float, optional):  dropout rate in Dropout layers. Defaults to 0.1.
+            optim (str, optional): if not None it expects a pytorch optim method. Defaults to None that is mapped to Adam.
+            optim_config (dict, optional): configuration for Adam optimizer. Defaults to None.
+            scheduler_config (dict, optional): configuration for stepLR scheduler. Defaults to None.
+        """
+        if activation == 'torch.nn.SELU':
+            beauty_string('SELU do not require BN','info')
+            use_bn = False
+        if type(activation)==str:
+            activation = get_activation(activation)
+        else:
+            beauty_string('There is a bug in pytorch lightening, the constructior is called twice ','info')
+        
+   
+        super(Autoformer, self).__init__()
+        self.save_hyperparameters(logger=False)
+        self.use_quantiles = False
+        self.optim = optim
+        self.optim_config = optim_config
+        self.scheduler_config = scheduler_config
+        self.loss_type = loss_type
+        self.persistence_weight = persistence_weight 
+
+        if self.loss_type == 'mse':
+            self.loss = nn.MSELoss()
+        else:
+            self.loss = nn.L1Loss()
+
+        
+        self.seq_len = past_steps
+        self.label_len = label_len
+        self.pred_len = future_steps
+
+        # Decomp
+        self.decomp = series_decomp(kernel_size)
+
+        self.embs = []
+        emb_channels = 0
+        for k in embs:
+            self.embs.append(nn.Embedding(k+1,d_model))
+            emb_channels = d_model
+            
+        past_channels+=emb_channels
+        future_channels+=emb_channels
+        
+        self.linear_encoder = nn.Sequential(nn.Linear(past_channels,past_channels*2),activation() ,nn.Dropout(dropout_rate),nn.Linear(past_channels*2,d_model*2),activation() ,nn.Dropout(dropout_rate),nn.Linear(d_model*2,d_model))
+        
+        self.linear_decoder = nn.Sequential(nn.Linear(future_channels,future_channels*2),activation() ,nn.Dropout(dropout_rate),nn.Linear(future_channels*2,d_model*2),activation() ,nn.Dropout(dropout_rate),nn.Linear(d_model*2,d_model))
+        # Encoder
+        self.encoder = Encoder(
+            [
+                EncoderLayer(
+                    AutoCorrelationLayer(
+                        AutoCorrelation(False, factor, attention_dropout=dropout_rate,
+                                        output_attention=False),
+                        d_model, n_head),
+                    d_model,
+                    hidden_size,
+                    moving_avg=kernel_size,
+                    dropout=dropout_rate,
+                    activation=activation
+                ) for l in range(n_layer_encoder)
+            ],
+            norm_layer=my_Layernorm(d_model)
+        )
+        # Decoder
+        self.decoder = Decoder(
+            [
+                DecoderLayer(
+                    AutoCorrelationLayer(
+                        AutoCorrelation(True, factor, attention_dropout=dropout_rate,
+                                        output_attention=False),
+                        d_model, n_head),
+                    AutoCorrelationLayer(
+                        AutoCorrelation(False, factor, attention_dropout=dropout_rate,
+                                        output_attention=False),
+                        d_model, n_head),
+                    d_model,
+                    out_channels,
+                    hidden_size,
+                    moving_avg=kernel_size,
+                    dropout=dropout_rate,
+                    activation=activation,
+                )
+                for l in range(n_layer_decoder)
+            ],
+            norm_layer=my_Layernorm(d_model),
+            projection=nn.Linear(d_model, out_channels, bias=True)
+        )
+        self.pee = PositionalEmbedding(d_model=d_model)
+        self.ped = PositionalEmbedding(d_model=d_model)
+
+    def forward(self, batch):
+        self.linear_encoder.device = self.device
+        self.linear_decoder.device = self.device
+
+        idx_target = batch['idx_target'][0]
+        idx_target_future = batch['idx_target_future'][0]
+        x_seq = batch['x_num_past'].to(self.device)#[:,:,idx_target]
+        
+        
+        
+        if 'x_cat_future' in batch.keys():
+            cat_future = batch['x_cat_future'].to(self.device)
+        if 'x_cat_past' in batch.keys():
+            cat_past = batch['x_cat_past'].to(self.device)
+        if 'x_num_future' in batch.keys():
+            x_future = batch['x_num_future'].to(self.device)
+        
+        x_future[:,:-self.pred_len,idx_target_future] = -100
+        
+        
+
+        pee = self.pee(x_seq)
+        ped = self.ped(torch.zeros(x_seq.shape[0], self.pred_len+self.label_len).float())
+        if 'x_cat_past' in batch.keys():
+            for i in range(len(self.embs)):
+                if i>0:
+                    tmp_emb+=self.embs[i](cat_past[:,:,i])
+                else:
+                    tmp_emb=self.embs[i](cat_past[:,:,i])
+            pee+=tmp_emb
+    
+        
+        
+        if 'x_cat_future' in batch.keys():
+            for i in range(len(self.embs)):
+                if i>0:
+                    tmp_emb+=self.embs[i](cat_future[:,:,i])
+                else:
+                    tmp_emb=self.embs[i](cat_future[:,:,i])
+            ped+=tmp_emb
+            
+
+        
+        mean = torch.mean(x_seq, dim=1).unsqueeze(1).repeat(1, self.pred_len, 1)
+
+        zeros = torch.zeros([x_future.shape[0], self.pred_len, x_seq.shape[2]], device=x_seq.device)
+        seasonal_init, trend_init = self.decomp(x_seq)
+        # decoder input
+        trend_init = torch.cat([trend_init[:, -self.label_len:, :], mean], dim=1)
+        seasonal_init = torch.cat([seasonal_init[:, -self.label_len:, :], zeros], dim=1)
+        # enc
+        enc_out = self.linear_encoder(x_seq)+pee
+        enc_out, attns = self.encoder(enc_out, attn_mask=None)
+        # dec
+        dec_out = self.linear_decoder(x_future)+ped
+        seasonal_part, trend_part = self.decoder(dec_out, enc_out, x_mask=None, cross_mask=None, trend=trend_init)
+        # final
+        dec_out = trend_part + seasonal_part
+
+
+  
+        return dec_out[:, -self.pred_len:, :].unsqueeze(3)  # [B, L, D]
+         
+        
---
+++ dsipts/models/PatchTST.py
@@ -0,0 +1,173 @@
+
+from torch import  nn
+import torch
+from .base import Base
+from typing import List,Union
+from ..data_structure.utils import beauty_string
+from .utils import  get_activation
+from .patchtst.layers import series_decomp, PatchTST_backbone
+
+
+  
+class PatchTST(Base):
+
+    
+    def __init__(self, 
+                 past_steps:int,
+                 future_steps:int,
+                 patch_len: int,
+                 past_channels:int,
+                 future_channels:int,
+                 out_channels:int,
+                 d_model:int,
+                 embs:List[int],
+                 kernel_size:int,
+                 decomposition:bool=True,
+                 activation:str='torch.nn.ReLU',
+                 n_head:int=1,
+                 n_layer:int=2,
+                 stride:int=8,
+                 remove_last:bool = False,
+                 hidden_size:int=1048,
+                 persistence_weight:float=0.0,
+                 loss_type: str='l1',
+                 quantiles:List[int]=[],
+                 dropout_rate:float=0.1,
+                 optim:Union[str,None]=None,
+                 optim_config:dict=None,
+                 scheduler_config:dict=None)->None:
+        """
+
+        Args:
+            past_steps (int): number of past datapoints used , not used here
+            future_steps (int): number of future lag to predict
+            patch_len (int): patch_len
+            past_channels (int): number of numeric past variables, must be >0
+            future_channels (int): number of future numeric variables 
+            out_channels (int):  number of output channels
+            d_model (int):  dimension of the attention model
+            embs (List): list of the initial dimension of the categorical variables
+            embed_type (int): type of embedding
+            kernel_size (int): kernel_size
+            activation (str, optional): activation fuction function pytorch. Default torch.nn.ReLU
+            n_head (int, optional): number of heads
+            n_layer (int, optional): number of encoding layers
+            
+            remove_last (boolean,optional): if true the model try to predic the difference respect the last observation.
+            
+            out_channels (int):  number of output channels
+            persistence_weight (float):  weight controlling the divergence from persistence model. Default 0
+            loss_type (str, optional): this model uses custom losses or l1 or mse. Custom losses can be linear_penalization or exponential_penalization. Default l1,
+            quantiles (List[int], optional): NOT USED YET
+            dropout_rate (float, optional):  dropout rate in Dropout layers. Defaults to 0.1.
+            optim (str, optional): if not None it expects a pytorch optim method. Defaults to None that is mapped to Adam.
+            optim_config (dict, optional): configuration for Adam optimizer. Defaults to None.
+            scheduler_config (dict, optional): configuration for stepLR scheduler. Defaults to None.
+        """
+        if activation == 'torch.nn.SELU':
+            beauty_string('SELU do not require BN','info')
+            use_bn = False
+        if type(activation)==str:
+            activation = get_activation(activation)
+        else:
+            beauty_string('There is a bug in pytorch lightening, the constructior is called twice ','info')
+        
+   
+        super(PatchTST, self).__init__()
+        self.save_hyperparameters(logger=False)
+        self.use_quantiles = False
+        self.optim = optim
+        self.optim_config = optim_config
+        self.scheduler_config = scheduler_config
+        self.loss_type = loss_type
+        self.persistence_weight = persistence_weight 
+        self.remove_last = remove_last
+        self.future_steps = future_steps  ##this is mandatory
+        if self.loss_type == 'mse':
+            self.loss = nn.MSELoss()
+        else:
+            self.loss = nn.L1Loss()
+
+    
+
+        self.embs = []
+        emb_channels = 0
+        for k in embs:
+            self.embs.append(nn.Embedding(k+1,d_model))
+            emb_channels = d_model
+            
+        past_channels+=emb_channels
+        
+        
+
+        
+        
+        # model
+        self.decomposition = decomposition
+        if self.decomposition:
+            self.decomp_module = series_decomp(kernel_size)
+            self.model_trend = PatchTST_backbone(c_in=past_channels, context_window = past_steps, target_window=future_steps, patch_len=patch_len, stride=stride, 
+                                  max_seq_len=past_steps+future_steps, n_layers=n_layer, d_model=d_model,
+                                  n_heads=n_head, d_k=None, d_v=None, d_ff=hidden_size, norm=False, attn_dropout=dropout_rate,
+                                  dropout=dropout_rate, act=activation(), key_padding_mask='auto', padding_var=None, 
+                                  attn_mask=None, res_attention=True, pre_norm=False, store_attn=False,
+                                  pe='zeros', learn_pe=True, fc_dropout=dropout_rate, head_dropout=dropout_rate, padding_patch = 'end',
+                                  pretrain_head=False, head_type='flatten', individual=False, revin=True, affine=False,
+                                  subtract_last=remove_last, verbose=False)
+            self.model_res = PatchTST_backbone(c_in=past_channels, context_window = past_steps, target_window=future_steps, patch_len=patch_len, stride=stride, 
+                                  max_seq_len=past_steps+future_steps, n_layers=n_layer, d_model=d_model,
+                                  n_heads=n_head, d_k=None, d_v=None, d_ff=hidden_size, norm=False, attn_dropout=dropout_rate,
+                                  dropout=dropout_rate, act=activation(), key_padding_mask='auto', padding_var=None, 
+                                  attn_mask=None, res_attention=True, pre_norm=False, store_attn=False,
+                                  pe='zeros', learn_pe=True, fc_dropout=dropout_rate, head_dropout=dropout_rate, padding_patch = 'end',
+                                  pretrain_head=False, head_type='flatten', individual=False, revin=True, affine=False,
+                                  subtract_last=remove_last, verbose=False)
+        else:
+            self.model = PatchTST_backbone(c_in=past_channels, context_window = past_steps, target_window=future_steps, patch_len=patch_len, stride=stride, 
+                                  max_seq_len=past_steps+future_steps, n_layers=n_layer, d_model=d_model,
+                                  n_heads=n_head, d_k=None, d_v=None, d_ff=hidden_size, norm=False, attn_dropout=dropout_rate,
+                                  dropout=dropout_rate, act=activation(), key_padding_mask='auto', padding_var=None, 
+                                  attn_mask=None, res_attention=True, pre_norm=False, store_attn=False,
+                                  pe='zeros', learn_pe=True, fc_dropout=dropout_rate, head_dropout=dropout_rate, padding_patch = 'end',
+                                  pretrain_head=False, head_type='flatten', individual=False, revin=True, affine=False,
+                                  subtract_last=remove_last, verbose=False)
+    
+        self.final_linear = nn.Sequential(nn.Linear(past_channels,past_channels//2),activation(),nn.Dropout(dropout_rate), nn.Linear(past_channels//2,out_channels)  )
+    
+    def forward(self, batch):           # x: [Batch, Input length, Channel]
+        
+        x_seq = batch['x_num_past'].to(self.device)#[:,:,idx_target]
+        if 'x_cat_past' in batch.keys():
+            cat_past = batch['x_cat_past'].to(self.device)
+        tot = [x_seq]
+        if 'x_cat_past' in batch.keys():
+            for i in range(len(self.embs)):
+                if i>0:
+                    tmp_emb+=self.embs[i](cat_past[:,:,i])
+                else:
+                    tmp_emb=self.embs[i](cat_past[:,:,i])
+            tot.append(tmp_emb)
+
+        x_seq = torch.cat(tot,axis=1)
+            
+            
+            
+        if self.decomposition:
+            res_init, trend_init = self.decomp_module(x_seq)
+            res_init, trend_init = res_init.permute(0,2,1), trend_init.permute(0,2,1)  # x: [Batch, Channel, Input length]
+            res = self.model_res(res_init)
+            trend = self.model_trend(trend_init)
+            x = res + trend
+            x = x.permute(0,2,1)    # x: [Batch, Input length, Channel]
+        else:
+            x = x.permute(0,2,1)    # x: [Batch, Channel, Input length]
+            x = self.model(x)
+            x = x.permute(0,2,1)    # x: [Batch, Input length, Channel]
+            
+        x = self.final_linear(x)       
+    
+        return x.unsqueeze(3)
+        
+        
+        
+        
