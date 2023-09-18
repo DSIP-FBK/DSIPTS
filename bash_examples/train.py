35f630cc1c19f2478e795ca0f1f6598b4b437704
anmartinelli <anmartinelli@fbk.eu>
2023-09-18 13:41:05 +0200

    Diffusion train.py

--- bash_examples/train.py
+++ bash_examples/train.py
@@ -122,7 +122,7 @@ def train(conf: DictConfig) -> None:
     elif conf.model.type == 'tft2':
         model =  TFT2(**model_conf,   optim_config = conf.optim_config,
                           scheduler_config =conf.scheduler_config )  
-    elif conf.model.type == 'diffusion_tft':
+    elif conf.model.type == 'diffusion':
         model =  TFT2(**model_conf,   optim_config = conf.optim_config,
                           scheduler_config =conf.scheduler_config )  
         
