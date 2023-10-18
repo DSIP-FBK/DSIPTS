93d9abcb233e535f635ec33f85b9a38233afad82
anmartinelli <anmartinelli@fbk.eu>
2023-10-18 11:08:49 +0200

    diffusion 2.0 pdb

--- dsipts/models/Diffusion.py
+++ dsipts/models/Diffusion.py
@@ -596,7 +596,9 @@ def _extract_into_tensor(arr, timesteps, broadcast_shape):
                             dimension equal to the length of timesteps.
     :return: a tensor of shape [batch_size, 1, ...] where the shape has K dims.
     """
-    res = torch.from_numpy(arr).to(device=timesteps.device)[timesteps].float()
+    import pdb
+    pdb.set_trace()
+    res = torch.from_numpy(arr).to(device=arr.device)[timesteps].float()
     while len(res.shape) < len(broadcast_shape):
         res = res[..., None]
     return res.expand(broadcast_shape)
