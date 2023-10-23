c387b706ef44c80d3b051d708997153b81fa6696
Andrea Gobbi <agobbi@fbk.eu>
2023-10-23 16:41:04 +0200

    ag update

--- bash_examples/load_data/load_data_public.py
+++ bash_examples/load_data/load_data_public.py
@@ -3,5 +3,8 @@ from dsipts import TimeSeries,read_public_dataset
 def load_data(conf):
     data, columns = read_public_dataset(**conf.dataset)
     ts = TimeSeries(conf.ts.name)
-    ts.load_signal(data, enrich_cat= conf.ts.enrich,target_variables=['y'], past_variables=columns if conf.ts.use_covariates else [],silly_model=conf.ts.get('silly',False))
+    ts.load_signal(data, enrich_cat= conf.ts.enrich,target_variables=['y'],
+                   past_variables=columns if conf.ts.use_covariates else [],
+                   future_variables=columns if conf.ts.use_future_covariates else [],
+                   silly_model=conf.ts.get('silly',False))
     return ts
