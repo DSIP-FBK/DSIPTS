ef3a65d3f645183cb020c732c44485731ee5fc5d
anmartinelli <anmartinelli@fbk.eu>
2023-10-23 14:12:36 +0200

    diffusion 2.0 debug var

--- dsipts/models/Diffusion.py
+++ dsipts/models/Diffusion.py
@@ -93,7 +93,7 @@ class Diffusion(Base):
             self.alphas_cumprod = f_cos_t/f_cos_t[0] # scaled cumulative product of alphas 
             self.alphas_cumprod_prev = np.append(1.0, self.alphas_cumprod[:-1]) # auxiliar vector to get easily alphaBAT_t-1 
             self.alphas = self.alphas_cumprod * (self.alphas_cumprod_prev)**(-1)
-            self.betas = 1-self.alphas
+            self.betas = np.append(self.s, 1-self.alphas[1:])
         else:
             # STANDARD ALPHA
             # beta is considered constant in [0,1) for all time steps. Good values near 0.03
