4a37c11e5badf20c77f24461f39c145293a90259
agobbi <agobbi@fbk.eu>
2024-04-30 11:04:00 +0200

    ag bf

--- bash_examples/compare.py
+++ bash_examples/compare.py
@@ -63,7 +63,7 @@ def compare(conf:DictConfig)-> None:
                 losses['model'] = f'{conf_tmp.model.type}_{conf_tmp.ts.name}_{conf_tmp.ts.version}'
             else:
                 beauty_string(f'Can not load losses {conf_tmp.model.type}_{conf_tmp.ts.name}_{conf_tmp.ts.version} maybe the train procedure is not completed','block',True)
-                beauty_string(f'ERROR:{traceback.format_exc()}')
+                beauty_string(f'ERROR:{traceback.format_exc()}','block',True)
 
             losses.value = np.log(losses.value)
             res.append(tmp )
