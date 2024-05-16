23e122244c524475fc822b8cd0694e3bea15d1a7
agobbi <agobbi@fbk.eu>
2024-05-16 17:08:51 +0200

    ag added cos sin day ale

--- bash_examples/load_data/load_data_temps.py
+++ bash_examples/load_data/load_data_temps.py
@@ -8,8 +8,8 @@ import pandas as pd
 def load_data(conf):
     data = np.load(os.path.join(conf.dataset.path,'temperature.npy'))
     data = pd.DataFrame({'time':range(len(data)),'y':data})
-    data['cos_day'] = np.cos(2*np.pi*range(len(data))/364)
-    data['sin_day'] = np.cos(2*np.pi*range(len(data))/364)
+    data['cos_day'] = np.cos(2*np.pi*list(range(len(data)))/364)
+    data['sin_day'] = np.cos(2*np.pi*list(range(len(data)))/364)
 
     ts = TimeSeries(conf.ts.name)
     ts.load_signal(data,past_variables =['y','cos_day','sin_day'],future_variables = ['cos_day','sin_day'],target_variables =['y'],enrich_cat= [],silly_model=conf.ts.get('silly',False))
