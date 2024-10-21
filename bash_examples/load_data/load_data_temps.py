ce7c0bcad4e27a18d9f57959c2e0814044d58e03
agobbi <agobbi@fbk.eu>
2024-10-21 10:36:17 +0200

    ag update anomalies

--- bash_examples/load_data/load_data_temps.py
+++ bash_examples/load_data/load_data_temps.py
@@ -6,14 +6,22 @@ import pandas as pd
  
 
 def load_data(conf):
+    
+    
+    
+    '''
     data = np.load(os.path.join(conf.dataset.path,'temperature.npy'))
     data = pd.DataFrame({'time':range(len(data)),'y':data})
     data['cos_day'] = np.cos(2*np.pi*np.arange(0,data.shape[0],1)/364)
     data['sin_day'] = np.cos(2*np.pi*np.arange(0,data.shape[0],1)/364)
-
+    '''
+    data = pd.read_csv(os.path.join(conf.dataset.path,'press_temp_indexes_1940_2023.csv'))# '/home/agobbi/Scaricati/press_temp_indexes_1940_2023.csv')
+    data['time'] = range( data.shape[0])
+    y = ['temp_index_1', 'temp_index_2', 'temp_index_3']
+    past = [x for x in data.columns if 'temp' in x] + [x for x in data.columns if 'press' in x]
+    
     ts = TimeSeries(conf.ts.name)
-    ts.load_signal(data,past_variables =['y','cos_day','sin_day'],future_variables = ['cos_day','sin_day'],target_variables =['y'],enrich_cat= [],silly_model=conf.ts.get('silly',False))
-
+    ts.load_signal(data,past_variables = past ,future_variables = [],target_variables = y,enrich_cat= [],silly_model=conf.ts.get('silly',False))
  
 
     return ts
