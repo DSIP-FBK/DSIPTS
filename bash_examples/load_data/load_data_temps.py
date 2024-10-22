ff18bed4d72c1e5f5d1edea81095588839083bbf
agobbi <agobbi@fbk.eu>
2024-10-22 16:19:42 +0200

    ag update temps

--- bash_examples/load_data/load_data_temps.py
+++ bash_examples/load_data/load_data_temps.py
@@ -17,11 +17,12 @@ def load_data(conf):
     '''
     data = pd.read_csv(os.path.join(conf.dataset.path,'press_temp_indexes_1940_2023.csv'))# '/home/agobbi/Scaricati/press_temp_indexes_1940_2023.csv')
     data['time'] = range( data.shape[0])
+    data['mm'] = data['month'].values
     y = ['temp_index_1']
     past = [x for x in data.columns if 'temp' in x] + [x for x in data.columns if 'press' in x]
     
     ts = TimeSeries(conf.ts.name)
-    ts.load_signal(data,past_variables = past ,future_variables = [],target_variables = y,cat_var= ['month'],silly_model=conf.ts.get('silly',False))
+    ts.load_signal(data,past_variables = past ,future_variables = [],target_variables = y,cat_var= ['mm'],silly_model=conf.ts.get('silly',False))
  
 
     return ts
