5e0718c0e36f46c77087cfadb968f10b1a774a9b
agobbi <agobbi@fbk.eu>
2024-10-22 16:17:31 +0200

    ag update temps

--- bash_examples/load_data/load_data_temps.py
+++ bash_examples/load_data/load_data_temps.py
@@ -21,7 +21,7 @@ def load_data(conf):
     past = [x for x in data.columns if 'temp' in x] + [x for x in data.columns if 'press' in x]
     
     ts = TimeSeries(conf.ts.name)
-    ts.load_signal(data,past_variables = past ,future_variables = [],target_variables = y,enrich_cat= ['month'],silly_model=conf.ts.get('silly',False))
+    ts.load_signal(data,past_variables = past ,future_variables = [],target_variables = y,cat_var= ['month'],silly_model=conf.ts.get('silly',False))
  
 
     return ts
