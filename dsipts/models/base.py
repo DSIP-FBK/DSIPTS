840ad5f40cf69e5ab4b6070c39bfe1c92997ecd3
agobbi <agobbi@fbk.eu>
2025-02-05 12:08:32 +0100

    ag debug

--- dsipts/models/base.py
+++ dsipts/models/base.py
@@ -145,8 +145,8 @@ class Base(pl.LightningModule):
         y_hat = self(batch)
         loss = self.compute_loss(batch,y_hat)
         if self.has_sam_optim:
-            #import pdb
-            #pdb.set_trace()
+            import pdb
+            pdb.set_trace()
             opt = self.optimizers()
             self.manual_backward(loss)
             opt.first_step(zero_grad=True)
