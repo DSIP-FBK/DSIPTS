a391fd83d55796c93016f4a24da01a469b9f1ac0
agobbi <agobbi@fbk.eu>
2025-11-10 14:21:56 +0100

    update

--- dsipts/src/dsipts/models/duet/layers.py
+++ dsipts/src/dsipts/models/duet/layers.py
@@ -436,6 +436,6 @@ class Linear_extractor_cluster(nn.Module):
         expert_outputs = [
             self.experts[i](expert_inputs[i]) for i in range(self.num_experts)
         ]
-        y = dispatcher.combine([e for e in expert_outputs if len(e)>0])
-
+        #y = dispatcher.combine([e for e in expert_outputs if len(e)>0])
+        y = dispatcher.combine(expert_outputs)
         return y, loss
