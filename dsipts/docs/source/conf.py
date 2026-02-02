c79ee93e255b93fd3f21279ac85bd1d0f8f7ff12
agobbi <agobbi@fbk.eu>
2026-02-02 16:50:32 +0100

    chore: bump version 1.1.31

--- dsipts/docs/source/conf.py
+++ dsipts/docs/source/conf.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.abspath("../../src"))
 # -- Project information -----------------------------------------------------
 project = "DSIPTS"
 author = "Andrea Gobbi (agobbifbk.eu)"
-release = "1.1.30"
+release = "1.1.31"
 
 # -- General configuration ---------------------------------------------------
 extensions = [
